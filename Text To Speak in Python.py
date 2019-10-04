86c19512632d759f093af7707995d3b494663323
Herberth Souza <51496746+Brevetecno@users.noreply.github.com>
2019-10-04 08:13:49 -0300

    Texto para Voz com Python
    Este é um simples código em Python 3 usando a biblioteca pyttsx3 para trasnformar textos em voz, sendo assim um sitentizador de voz sem precisar de Internet

--- Text To Speak in Python.py
+++ Text To Speak in Python.py
@@ -3,11 +3,13 @@
 # Python 3
 # 2019
 
+# Biblioteca
 import pyttsx3 as tts
 
 # Inicia o sintetizador de voz
 voz = tts.init()
 
+# Loop
 while True:
     print('='*50)
 
@@ -15,9 +17,13 @@ while True:
     cmd = str(input('Falar: '))
 
     try:
+        # Adiciona o que deve ser dito
         voz.say(cmd)
+        
+        # Reproduzir o comando transformado em voz
         voz.runAndWait()
     except:
+        # Caso aconteça algum erro
         print('Ocorreu algum erro durante o processo.')
     
     print('='*50)
